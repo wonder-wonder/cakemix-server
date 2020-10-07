6032a1c31a8f6dc3cd85c75a0d39b87edffb4925
wancom <softpiware@gmail.com>
2020-10-07 10:37:34 +0900

    Support multi session

--- handler/ot.go
+++ handler/ot.go
@@ -13,11 +13,9 @@ import (
 	"github.com/wonder-wonder/cakemix-server/ot"
 )
 
-var clients []ClientInfo
-
 type ClientInfo struct {
 	Conn      *websocket.Conn `json:"-"`
-	ID        int             `json:"-"`
+	ID        string          `json:"-"`
 	Name      string          `json:"name"`
 	Selection []SelData       `json:"selection"`
 }
@@ -118,8 +116,8 @@ func ParseSel(msg []byte) ([]SelData, error) {
 	return rang.Ranges, nil
 }
 
-func Broadcast(from int, msg []byte) {
-	for _, c := range clients {
+func Broadcast(sess *Session, from string, msg []byte) {
+	for _, c := range sess.Clinets {
 		if c.ID == from {
 			continue
 		}
@@ -127,108 +125,120 @@ func Broadcast(from int, msg []byte) {
 	}
 }
 
+type Session struct {
+	Clinets map[string]ClientInfo
+	OT      *ot.OT
+}
+
+// var clients []ClientInfo
+var sessions = map[string]Session{}
+
 func (h *Handler) OTHandler(r *gin.RouterGroup) {
-	otsess := ot.New("Hello, world!")
+	// TODO: user check
+	r.GET("/ws", getOTHandler)
+}
+func getOTHandler(c *gin.Context) {
+	did := c.Query("docid")
+	// TODO: get user name
+	name := "guest"
 
-	r.GET("/ws", func(c *gin.Context) {
-		var wsupgrader = websocket.Upgrader{
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-		}
-		conn, err := wsupgrader.Upgrade(c.Writer, c.Request, nil)
+	var wsupgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	conn, err := wsupgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Failed to set websocket upgrade: %v", err)
+		return
+	}
+
+	sess, ok := sessions[did]
+	if !ok {
+		//Load document
+		text := "Hello, world!"
+		sess = Session{Clinets: map[string]ClientInfo{}, OT: ot.New(text)}
+		sessions[did] = sess
+	}
+
+	docdatraw, err := json.Marshal(DocData{Clients: sess.Clinets, Document: sess.OT.Text, Revision: len(sess.OT.History)})
+	if err != nil {
+		panic(err)
+	}
+	initDocRaw, err := json.Marshal(WSMsg{Event: "doc", Data: docdatraw})
+	if err != nil {
+		panic(err)
+	}
+	conn.WriteMessage(websocket.TextMessage, initDocRaw)
+	// useridint := len(sess.Clinets)
+	userid := strconv.Itoa(len(sess.Clinets))
+	sess.Clinets[userid] = ClientInfo{Conn: conn, ID: userid, Name: name}
+	for {
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
-			log.Println("Failed to set websocket upgrade: %v", err)
-			return
+			break
 		}
-		cl := map[string]ClientInfo{}
-		for i, c := range clients {
-			cl[strconv.Itoa(i)] = c
-		}
-		docdatraw, err := json.Marshal(DocData{Clients: cl, Document: otsess.Text, Revision: len(otsess.History)})
+
+		dat := WSMsg{}
+		err = json.Unmarshal(msg, &dat)
 		if err != nil {
 			panic(err)
 		}
-		initDocRaw, err := json.Marshal(WSMsg{Event: "doc", Data: docdatraw})
-		if err != nil {
-			panic(err)
-		}
-		conn.WriteMessage(websocket.TextMessage, initDocRaw)
-		useridint := len(clients)
-		userid := strconv.Itoa(useridint)
-		name := "guest"
-		if useridint == 0 {
-			name = "alice"
-		} else if useridint == 1 {
-			name = "bob"
-		}
-		clients = append(clients, ClientInfo{Conn: conn, ID: useridint, Name: name})
-		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				break
-			}
-
-			dat := WSMsg{}
-			err = json.Unmarshal(msg, &dat)
+		if dat.Event == "op" {
+			opdat, err := ParseOp(dat.Data, userid)
 			if err != nil {
 				panic(err)
 			}
-			if dat.Event == "op" {
-				opdat, err := ParseOp(dat.Data, userid)
-				if err != nil {
-					panic(err)
-				}
 
-				op, err := otsess.Operate(opdat.Revision, opdat.Operation)
-				opraw := []interface{}{}
-				for _, v := range op.Ops {
-					if v.OpType == ot.OpTypeRetain {
-						opraw = append(opraw, v.Len)
-					} else if v.OpType == ot.OpTypeInsert {
-						opraw = append(opraw, v.Text)
-					} else if v.OpType == ot.OpTypeDelete {
-						opraw = append(opraw, -v.Len)
-					}
+			op, err := sess.OT.Operate(opdat.Revision, opdat.Operation)
+			opraw := []interface{}{}
+			for _, v := range op.Ops {
+				if v.OpType == ot.OpTypeRetain {
+					opraw = append(opraw, v.Len)
+				} else if v.OpType == ot.OpTypeInsert {
+					opraw = append(opraw, v.Text)
+				} else if v.OpType == ot.OpTypeDelete {
+					opraw = append(opraw, -v.Len)
 				}
-				opres := []interface{}{op.User, opraw, map[string][]SelData{"ranges": opdat.Selection}}
-				opresraw, err := json.Marshal(opres)
-				if err != nil {
-					panic(err)
-				}
-				dat.Data = opresraw
-				datraw, err := json.Marshal(dat)
-				if err != nil {
-					panic(err)
-				}
-				Broadcast(useridint, datraw)
-				fmt.Printf("\n%s\n", otsess.Text)
-				temp := `{"e":"ok"}`
-				conn.WriteMessage(websocket.TextMessage, []byte(temp))
-			} else if dat.Event == "sel" {
-				sel, err := ParseSel(dat.Data)
-				if err != nil {
-					panic(err)
-				}
-				clients[useridint].Selection = sel
-
-				selres := []interface{}{userid, map[string][]SelData{"ranges": sel}}
-				selresraw, err := json.Marshal(selres)
-				if err != nil {
-					panic(err)
-				}
-				dat.Data = selresraw
-				datraw, err := json.Marshal(dat)
-				if err != nil {
-					panic(err)
-				}
-				Broadcast(useridint, datraw)
 			}
+			opres := []interface{}{op.User, opraw, map[string][]SelData{"ranges": opdat.Selection}}
+			opresraw, err := json.Marshal(opres)
+			if err != nil {
+				panic(err)
+			}
+			dat.Data = opresraw
+			datraw, err := json.Marshal(dat)
+			if err != nil {
+				panic(err)
+			}
+			Broadcast(&sess, userid, datraw)
+			fmt.Printf("\n%s\n", sess.OT.Text)
+			temp := `{"e":"ok"}`
+			conn.WriteMessage(websocket.TextMessage, []byte(temp))
+		} else if dat.Event == "sel" {
+			sel, err := ParseSel(dat.Data)
+			if err != nil {
+				panic(err)
+			}
+			cl, _ := sess.Clinets[userid]
+			cl.Selection = sel
+			sess.Clinets[userid] = cl
 
+			selres := []interface{}{userid, map[string][]SelData{"ranges": sel}}
+			selresraw, err := json.Marshal(selres)
+			if err != nil {
+				panic(err)
+			}
+			dat.Data = selresraw
+			datraw, err := json.Marshal(dat)
+			if err != nil {
+				panic(err)
+			}
+			Broadcast(&sess, userid, datraw)
 		}
 
-	})
+	}
 
 }
