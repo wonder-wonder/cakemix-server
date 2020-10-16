d77e3e4430c6757a7396fd29ea9da3bff6f4a247
wancom <softpiware@gmail.com>
2020-10-16 16:52:01 +0900

    Add init DB data and refact

---
+++ docker/postgres/12-initdata.sql
@@ -0,0 +1,23 @@
+\c cakemix;
+
+-- System Admin (default user)
+-- Email:root@localhost Pass:cakemix
+INSERT INTO username VALUES('ujafzavrqkqthqe54', 'root');
+INSERT INTO auth VALUES('ujafzavrqkqthqe54',	'root@localhost',	'DBerQ+J0ywuKJ+sSXHx9Y/5L4qxhL/275f3d70YjINmgj5ftoNL9yu42aujAEpUUTYiZZUpdqojuhRj7ry3ISQ==',	'nEmGLz2FIqoOJAsN');
+INSERT INTO profile VALUES('ujafzavrqkqthqe54','SystemAdmin','','',1,'','ja');
+
+-- System Admin Team
+INSERT INTO username VALUES('tqssoagvfvlg3mky2', 'systemadmin');
+INSERT INTO profile VALUES('tqssoagvfvlg3mky2','SystemAdmin Team','','',1,'','ja');
+INSERT INTO teammember VALUES('tqssoagvfvlg3mky2', 'ujafzavrqkqthqe54', 0, 1);
+
+-- Default Environments
+-- Default tag
+INSERT INTO tag VALUES (0,'notag');
+-- Root folder
+INSERT INTO folder VALUES('fwk6al7nyj4qdufaz','tqssoagvfvlg3mky2','','',0,1,1,'ujafzavrqkqthqe54');
+-- User folder
+INSERT INTO folder VALUES('fdahpbkboamdbgnua','tqssoagvfvlg3mky2','fwk6al7nyj4qdufaz','User',0,1,1,'ujafzavrqkqthqe54');
+
+-- Admin folder
+INSERT INTO folder VALUES('fhfprvdljyczssis7','ujafzavrqkqthqe54','fdahpbkboamdbgnua','root',0,1,1,'ujafzavrqkqthqe54');
---
+++ docker/postgres/20-sample.sql
@@ -0,0 +1,14 @@
+\c cakemix;
+
+-- Test user
+INSERT INTO username VALUES('ujuxj7nrznlg655jt', 'test');
+INSERT INTO auth VALUES('ujuxj7nrznlg655jt',	'test@example.com',	'CgL73Jxt1TcOIemWTZye7FkVNIz2kF+5cLG4RzAEsxhpow98YJGQr4yaK2IpbT5gwk097EGNcu/hi90nCZrN8w==',	'GlORyqKbYphmrv9I');
+INSERT INTO profile VALUES('ujuxj7nrznlg655jt','test','','',1595073002,'','ja');
+INSERT INTO folder VALUES('fde2uhiehs25fahxk','ujuxj7nrznlg655jt','fdahpbkboamdbgnua','test',0,1595073002,1595073002,'ujuxj7nrznlg655jt');
+
+INSERT INTO teammember VALUES('tqssoagvfvlg3mky2', 'ujuxj7nrznlg655jt', 0, 1595073002);
+
+insert into tag values (1,'tag1');
+insert into document values ('dotyrr3hpdeyvpwhy','ujuxj7nrznlg655jt','fde2uhiehs25fahxk','title',2,1595073002,1595073002,'ujuxj7nrznlg655jt',0);
+insert into documentrevision values ('dotyrr3hpdeyvpwhy','',1);
+insert into documentrevision values ('dotyrr3hpdeyvpwhy','This is a test.',1595073002);
