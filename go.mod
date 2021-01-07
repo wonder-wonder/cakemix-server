ee3c7d1cfd278a1f6fbbeb3ecb5cd5904ca055bf
wancom <softpiware@gmail.com>
2021-01-07 18:08:10 +0900

    Add auth test(login, logout)

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/lib/pq v1.8.0
 	github.com/sendgrid/rest v2.6.2+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.7.0+incompatible
+	github.com/stretchr/testify v1.6.1
 )
---
+++ handler/auth_test.go
@@ -0,0 +1,205 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/wonder-wonder/cakemix-server/db"
+)
+
+func TestAuthHandler(t *testing.T) {
+	os.Setenv("SIGNPRVKEY", "../signkey")
+	os.Setenv("SIGNPUBKEY", "../signkey.pub")
+
+	r, db, v1 := testInit(t)
+
+	h := NewHandler(db)
+	h.AuthHandler(v1)
+	token := ""
+
+	t.Run("Login", func(t *testing.T) {
+		type req struct {
+			body string
+		}
+		type res struct {
+			code int
+			// body string
+		}
+		tests := []struct {
+			name string
+			req  req
+			res  res
+		}{
+			{
+				name: "Root",
+				req: req{
+					body: `{"id":"root","pass":"cakemix"}`,
+				},
+				res: res{
+					code: 200,
+					// body: "",
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("POST", "/v1/auth/login", bytes.NewBufferString(tt.req.body))
+				r.ServeHTTP(w, req)
+				assert.Equal(t, tt.res.code, w.Code)
+
+				resraw := w.Body.Bytes()
+				if string(resraw) == "" {
+					t.Fatalf("should be string, got empty string")
+				}
+
+				var res map[string]string
+				err := json.Unmarshal(resraw, &res)
+				if !assert.NoError(t, err, "fail to umarshal json:\n%v", err) {
+					t.FailNow()
+				}
+				jwt, ok := res["jwt"]
+				if !assert.True(t, ok, "should has jwt, got:\n%v", res) {
+					t.FailNow()
+				}
+				assert.NotEmpty(t, jwt)
+				token = jwt
+				// var exp interface{}
+				// json.Unmarshal([]byte(tt.res.body), exp)
+				// assert.Equal(t, tt.res.body, w.Body.String())
+
+			})
+		}
+	})
+	t.Run("CheckToken", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("RegistGenToken", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("PassChange", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("Logout", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		type req struct {
+			header map[string]string
+		}
+		type res struct {
+			code int
+		}
+		tests := []struct {
+			name string
+			req  req
+			res  res
+		}{
+			{
+				name: "Root",
+				req: req{
+					header: map[string]string{"Authorization": `Bearer ` + token},
+				},
+				res: res{
+					code: 200,
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("POST", "/v1/auth/logout", nil)
+				for hk, hv := range tt.req.header {
+					req.Header.Set(hk, hv)
+				}
+				r.ServeHTTP(w, req)
+				assert.Equal(t, tt.res.code, w.Code)
+			})
+		}
+	})
+
+	t.Run("CheckUserName", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("GetRegistPre", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("PostRegistPre", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("RegistVerify", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+
+	t.Run("PassReset", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("GetPassResetVerify", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+	t.Run("PostPassResetVerify", func(t *testing.T) {
+		if token == "" {
+			t.SkipNow()
+		}
+		// TODO: impl test
+		t.Skip("Not implemented.")
+	})
+}
+
+func testInit(tb testing.TB) (*gin.Engine, *db.DB, *gin.RouterGroup) {
+	tb.Helper()
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	err := db.LoadKeys()
+	if err != nil {
+		tb.Errorf("testInit: %v", err)
+	}
+	db, err := db.OpenDB()
+	if err != nil {
+		tb.Errorf("testInit: %v", err)
+	}
+	v1 := r.Group("v1")
+	return r, db, v1
+}
