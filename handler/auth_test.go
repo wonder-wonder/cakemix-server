0e77945d3bce6509112448f49619012db0474604
wancom <softpiware@gmail.com>
2021-01-07 18:24:08 +0900

    Add auth/regist/gen/token test

--- handler/auth_test.go
+++ handler/auth_test.go
@@ -68,7 +68,9 @@ func TestAuthHandler(t *testing.T) {
 				if !assert.True(t, ok, "should has jwt, got:\n%v", res) {
 					t.FailNow()
 				}
-				assert.NotEmpty(t, jwt)
+				if !assert.NotEmpty(t, jwt) {
+					t.FailNow()
+				}
 				token = jwt
 				// var exp interface{}
 				// json.Unmarshal([]byte(tt.res.body), exp)
@@ -118,8 +120,56 @@ func TestAuthHandler(t *testing.T) {
 		if token == "" {
 			t.SkipNow()
 		}
-		// TODO: impl test
-		t.Skip("Not implemented.")
+		type req struct {
+			header map[string]string
+		}
+		type res struct {
+			code int
+		}
+		tests := []struct {
+			name string
+			req  req
+			res  res
+		}{
+			{
+				name: "Root",
+				req: req{
+					header: map[string]string{"Authorization": `Bearer ` + token},
+				},
+				res: res{
+					code: 200,
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("GET", "/v1/auth/regist/gen/token", nil)
+				for hk, hv := range tt.req.header {
+					req.Header.Set(hk, hv)
+				}
+				r.ServeHTTP(w, req)
+				assert.Equal(t, tt.res.code, w.Code)
+
+				resraw := w.Body.Bytes()
+				if !assert.NotEmpty(t, resraw, "should be string, got empty string") {
+					t.FailNow()
+				}
+
+				var res map[string]string
+				err := json.Unmarshal(resraw, &res)
+				if !assert.NoError(t, err, "fail to umarshal json:\n%v", err) {
+					t.FailNow()
+				}
+				invtoken, ok := res["token"]
+				if !assert.True(t, ok, "should has jwt, got:\n%v", res) {
+					t.FailNow()
+				}
+				if !assert.NotEmpty(t, invtoken) {
+					t.FailNow()
+				}
+			})
+		}
 	})
 	t.Run("PassChange", func(t *testing.T) {
 		if token == "" {
