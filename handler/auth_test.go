e523a31d2b9a1cf7c87d75c1cf8e9bb7cd24209f
wancom <softpiware@gmail.com>
2021-01-08 11:28:04 +0900

    Add post auth/pass/reset/verify test

--- handler/auth_test.go
+++ handler/auth_test.go
@@ -488,6 +488,7 @@ func TestAuthHandler(t *testing.T) {
 				if !assert.NotEmpty(t, veritoken) {
 					t.FailNow()
 				}
+
 				w := httptest.NewRecorder()
 				req, _ := http.NewRequest("GET", "/v1/auth/pass/reset/verify/"+veritoken, nil)
 				r.ServeHTTP(w, req)
@@ -501,7 +502,49 @@ func TestAuthHandler(t *testing.T) {
 		if token == "" {
 			t.SkipNow()
 		}
-		// TODO: impl test
-		t.Skip("Not implemented.")
+		type req struct {
+			email string
+			body  string
+		}
+		type res struct {
+			code int
+		}
+		tests := []struct {
+			name string
+			req  req
+			res  res
+		}{
+			{
+				name: "Test",
+				req: req{
+					email: "test@example.com",
+					body:  `{"newpass":"resetpass"}`,
+				},
+				res: res{
+					code: 200,
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				veritoken := ""
+				dateint := time.Now().Unix()
+				row := db.QueryRow("SELECT token FROM passreset,auth WHERE email = $1 AND expdate > $2 AND passreset.uuid = auth.uuid", tt.req.email, dateint)
+				err := row.Scan(&veritoken)
+				if !assert.NoError(t, err) {
+					t.FailNow()
+				}
+				if !assert.NotEmpty(t, veritoken) {
+					t.FailNow()
+				}
+
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("POST", "/v1/auth/pass/reset/verify/"+veritoken, bytes.NewBufferString(tt.req.body))
+				r.ServeHTTP(w, req)
+				if !assert.Equal(t, tt.res.code, w.Code) {
+					t.FailNow()
+				}
+			})
+		}
 	})
 }
