0968570deccfcd5bec0de1aa61284ae5eb37809a
wancom <softpiware@gmail.com>
2021-01-17 10:50:56 +0900

    Add put team/teamid/member test

--- handler/team_test.go
+++ handler/team_test.go
@@ -110,8 +110,50 @@ func TestTeamHandler(t *testing.T) {
 		if token == "" {
 			t.SkipNow()
 		}
-		// TODO: impl test
-		t.Skip("Not implemented.")
+
+		type req struct {
+			header   map[string]string
+			teamname string
+			body     string
+		}
+		type res struct {
+			code int
+		}
+		tests := []struct {
+			name string
+			req  req
+			res  res
+		}{
+			{
+				name: "TestTeam",
+				req: req{
+					header:   map[string]string{"Authorization": `Bearer ` + token},
+					teamname: "testteam",
+					body:     `{"member":"urtsqctxpdg3ypzan","permission":1}`,
+				},
+				res: res{
+					code: 200,
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				teamid := ""
+				row := db.QueryRow("SELECT uuid FROM username WHERE username = $1", tt.req.teamname)
+				err := row.Scan(&teamid)
+				assert.NoError(t, err)
+
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("PUT", "/v1/team/"+teamid+"/member", bytes.NewBufferString(tt.req.body))
+				for hk, hv := range tt.req.header {
+					req.Header.Set(hk, hv)
+				}
+				r.ServeHTTP(w, req)
+				if !assert.Equal(t, tt.res.code, w.Code) {
+					t.FailNow()
+				}
+			})
+		}
 	})
 	t.Run("GetTeamMember", func(t *testing.T) {
 		if token == "" {
