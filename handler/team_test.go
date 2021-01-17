de56f1afb0c23fe60d70e0cd0d7e7196c8b80e1d
wancom <softpiware@gmail.com>
2021-01-17 09:56:23 +0900

    Add post team test

--- handler/team_test.go
+++ handler/team_test.go
@@ -1,19 +1,58 @@
 package handler
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTeamHandler(t *testing.T) {
 	r := testInit(t)
 	token := testGetToken(t, r)
 
-	t.Run("GetTeamMember", func(t *testing.T) {
+	t.Run("CreateTeam", func(t *testing.T) {
 		if token == "" {
 			t.SkipNow()
 		}
-		// TODO: impl test
-		t.Skip("Not implemented.")
+
+		type req struct {
+			header   map[string]string
+			teamname string
+		}
+		type res struct {
+			code int
+		}
+		tests := []struct {
+			name string
+			req  req
+			res  res
+		}{
+			{
+				name: "TestTeam",
+				req: req{
+					header:   map[string]string{"Authorization": `Bearer ` + token},
+					teamname: "testteam",
+				},
+				res: res{
+					code: 200,
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("POST", "/v1/team?name="+tt.req.teamname, nil)
+				for hk, hv := range tt.req.header {
+					req.Header.Set(hk, hv)
+				}
+				r.ServeHTTP(w, req)
+				if !assert.Equal(t, tt.res.code, w.Code) {
+					t.FailNow()
+				}
+			})
+		}
 	})
 	t.Run("AddTeamMember", func(t *testing.T) {
 		if token == "" {
@@ -29,14 +68,14 @@ func TestTeamHandler(t *testing.T) {
 		// TODO: impl test
 		t.Skip("Not implemented.")
 	})
-	t.Run("DeleteTeamMember", func(t *testing.T) {
+	t.Run("GetTeamMember", func(t *testing.T) {
 		if token == "" {
 			t.SkipNow()
 		}
 		// TODO: impl test
 		t.Skip("Not implemented.")
 	})
-	t.Run("CreateTeam", func(t *testing.T) {
+	t.Run("DeleteTeamMember", func(t *testing.T) {
 		if token == "" {
 			t.SkipNow()
 		}
