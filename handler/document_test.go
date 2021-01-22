c3498ebdf62625da82032aa9424be8c1a0a6648c
wancom <softpiware@gmail.com>
2021-01-22 22:26:59 +0900

    Add put doc/docid/move/targetfid test

--- handler/document_test.go
+++ handler/document_test.go
@@ -203,8 +203,42 @@ func TestDocumentHandler(t *testing.T) {
 		if newdid == "" {
 			t.SkipNow()
 		}
-		// TODO: impl test
-		t.Skip("Not implemented.")
+		type req struct {
+			header    map[string]string
+			targetfid string
+		}
+		type res struct {
+			code int
+		}
+		tests := []struct {
+			name string
+			req  req
+			res  res
+		}{
+			{
+				name: "Root",
+				req: req{
+					header:    map[string]string{"Authorization": `Bearer ` + token},
+					targetfid: "fhfprvdljyczssis7",
+				},
+				res: res{
+					code: 200,
+				},
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest("PUT", "/v1/doc/"+newdid+"/move/"+tt.req.targetfid, nil)
+				for hk, hv := range tt.req.header {
+					req.Header.Set(hk, hv)
+				}
+				r.ServeHTTP(w, req)
+				if !assert.Equal(t, tt.res.code, w.Code) {
+					t.FailNow()
+				}
+			})
+		}
 	})
 	t.Run("RemoveDoc", func(t *testing.T) {
 		if token == "" {
