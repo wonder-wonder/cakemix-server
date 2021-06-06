0135d500ef76f4992207c2d3e3dbdaf127d5abc8
wancom <softpiware@gmail.com>
2021-06-06 21:57:22 +0900

    Add get user lock API

--- handler/auth.go
+++ handler/auth.go
@@ -34,6 +34,7 @@ func (h *Handler) AuthHandler(r *gin.RouterGroup) {
 	authck.GET("session", h.getSessionHandler)
 	authck.DELETE("session/:id", h.removeSessionHandler)
 	authck.GET("log", h.getLogHandler)
+	authck.GET("lock/:uuid", h.getLockUserHandler)
 }
 
 func (h *Handler) loginHandler(c *gin.Context) {
@@ -493,4 +494,34 @@ func (h *Handler) getLogHandler(c *gin.Context) {
 	}
 
 	c.AbortWithStatusJSON(http.StatusOK, res)
+}
+
+func (h *Handler) getLockUserHandler(c *gin.Context) {
+	res := model.AuthLockRes{}
+	targetuuid := c.Param("uuid")
+
+	uuid, ok := getUUID(c)
+	if !ok {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	// Only admin can operate
+	isAdmin, err := h.db.IsAdmin(uuid)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	if !isAdmin {
+		c.AbortWithStatus(http.StatusForbidden)
+		return
+	}
+
+	res.Status, err = h.db.IsUserLocked(targetuuid)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
 }
